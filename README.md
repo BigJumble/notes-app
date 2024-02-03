cdf433dda6b7ce3f710acdb643555c7e113eabaa
EimantasV <eima.eima1244@gmail.com>
2024-02-03 21:51:27 +0200

    Formatting

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # notes-app
+
+## Setup
+
+- Run `npm i` to install dependencies.
+- Run `npm run start` to start the local server.
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+let serverRestarted = true;
+app.get('/api/refresh-status', (req, res) => {
+  res.json({ restarted: serverRestarted });
+  serverRestarted = false;
+});
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
